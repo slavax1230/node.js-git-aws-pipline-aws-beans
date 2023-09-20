5bcf2b9cc65b0a0eae8f6affe60b3444ed614838
slavax1230 <slavax25@gmail.com>
2023-09-20 08:29:07 -0700

    1.0

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json())
+
+
+app.listen(2000)
+
+console.log("node.js starting...")
+
+app.use("/", (req,res) => {
+    return res.status(200).json({ message: "Hello from AWS pipline"})
+})
